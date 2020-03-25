9dc9045e3c93912a6bc1c50027694a042a313bfa
vladcto <razrab.ytka@gmail.com>
2020-03-25 22:09:45 +0300

    Создал проект, добавил BottomNavigationBar и навигацию при помощи BottomNavigationBar.

---
+++ app/src/main/java/com/example/foodchoise/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.foodchoise;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+
+                switch (menuItem.getItemId()){
+                    case R.id.fast_choise:
+                        selectedFragment = new CardFragment();
+                        break;
+
+                    case R.id.recipes_menu:
+                        selectedFragment = new ReciepsFragment();
+                        break;
+                    case R.id.profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.recipes_menu);
+    }
+}
---
+++ app/src/main/java/com/example/foodchoise/ReciepsFragment.java
@@ -0,0 +1,19 @@
+package com.example.foodchoise;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ReciepsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recieps_page,container,false);
+    }
+}
