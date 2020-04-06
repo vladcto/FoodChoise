9a66890ac32bf7df392b32b73f881a7930159bae
vladcto <razrab.ytka@gmail.com>
2020-04-06 21:59:53 +0300

    Исправил step_instr_recipes_fragment , а также подключил RecyclerView.

--- app/src/main/java/com/example/foodchoise/step_classes/StepInstrFragment.java
+++ app/src/main/java/com/example/foodchoise/step_classes/StepInstrFragment.java
@@ -4,18 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodchoise.R;
 
+import timber.log.Timber;
+
 public class StepInstrFragment extends Fragment {
-    //TODO: Подключить Adapter к RecyclerView
+    final StepInstrIngridientAdapter adapter = new StepInstrIngridientAdapter();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.step_instr_recipes_fragment,container,false);
+        View view = inflater.inflate(R.layout.step_instr_recipes_fragment,container,false);
+        RecyclerView recyclerView = view.findViewById(R.id.inst_recyclerview);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        Button button = view.findViewById(R.id.add_instr_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timber.i("Add instruction.");
+                adapter.addItem();
+            }
+        });
+        return view;
     }
 }
