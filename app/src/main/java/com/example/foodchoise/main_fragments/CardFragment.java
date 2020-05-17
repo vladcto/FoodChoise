ba515e3f910664345fb864c0b6e1baf7dd23f403
vladcto <razrab.ytka@gmail.com>
2020-05-17 19:40:25 +0300

    Добавил возможность отслеживать была ли сброшена или принята карточка в stackView. Улучшил читаемость и архитектуру класса активити для карточек.

--- app/src/main/java/com/example/foodchoise/main_fragments/CardFragment.java
+++ app/src/main/java/com/example/foodchoise/main_fragments/CardFragment.java
@@ -15,24 +15,26 @@ import com.example.foodchoise.entity_classes.RecipeCard;
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackView;
 import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
 
 import java.util.ArrayList;
 
-import leakcanary.AppWatcher;
 import timber.log.Timber;
 
-public class CardFragment extends Fragment {
+
+public class CardFragment extends Fragment implements com.yuyakaido.android.cardstackview.CardStackListener {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.page_card,container,false);
+        View view = inflater.inflate(R.layout.page_card, container, false);
 
         //Настраиваем StackView
         CardStackView stackView = view.findViewById(R.id.cardStackView);
-        StackChoiceListener listener = new StackChoiceListener();
-        AppWatcher.INSTANCE.getObjectWatcher().watch(listener,"StackChoiceListener was detached");
-        CardStackLayoutManager cardStackLayoutManager = new CardStackLayoutManager(getContext(),listener);
+
+        CardStackLayoutManager cardStackLayoutManager = new CardStackLayoutManager(getContext(), this);
+        cardStackLayoutManager.setStackFrom(StackFrom.None);
+
         stackView.setLayoutManager(cardStackLayoutManager);
         CardStackViewAdapter adapter = new CardStackViewAdapter();
         stackView.setAdapter(adapter);
@@ -42,53 +44,68 @@ public class CardFragment extends Fragment {
         adapter.addRecipesCard(firestoreHelper.getRecipesCard());
          */
 
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
-        adapter.TEST_addRecipeCard(new RecipeCard("name","dssd",new ArrayList<String>(),new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
+        adapter.TEST_addRecipeCard(new RecipeCard("name", "dssd", new ArrayList<String>(), new ArrayList<String>()));
         return view;
     }
 
-    static class StackChoiceListener implements com.yuyakaido.android.cardstackview.CardStackListener{
+    //region Listener for StackView
+    @Override
+    public void onCardDragging(Direction direction, float ratio) {
+        Timber.v("Card Drag");
+    }
 
-        @Override
-        public void onCardDragging(Direction direction, float ratio) {
-            Timber.d("Card Drag");
-        }
-
-        @Override
-        public void onCardSwiped(Direction direction) {
-            Timber.d("Card Swiped");
-        }
-
-        @Override
-        public void onCardRewound() {
-            Timber.d("Card Rewound");
-        }
-
-        @Override
-        public void onCardCanceled() {
-            Timber.d("Card Canceled");
-        }
-
-        @Override
-        public void onCardAppeared(View view, int position) {
-            Timber.d("Card Appeared");
-        }
-
-        @Override
-        public void onCardDisappeared(View view, int position) {
-            Timber.d("Card Disappeared");
+    @Override
+    public void onCardSwiped(Direction direction) {
+        if (direction == Direction.Right) {
+            onCardAccepted();
+        } else if (direction == Direction.Left) {
+            onCardRejected();
+        } else {
+            Timber.w("Not realize direction = %s",direction);
         }
     }
+
+    @Override
+    public void onCardRewound() {
+        Timber.d("Card Rewound");
+    }
+
+    @Override
+    public void onCardCanceled() {
+        Timber.d("Card Canceled");
+    }
+
+    @Override
+    public void onCardAppeared(View view, int position) {
+        Timber.d("Card Appeared");
+    }
+
+    @Override
+    public void onCardDisappeared(View view, int position) {
+        Timber.d("Card Disappeared");
+    }
+
+    //Методы для обработки событий.
+    private void onCardAccepted(){
+        Timber.i("Card Accepted");
+    }
+
+    private void onCardRejected(){
+        Timber.i("Card Rejected");
+    }
+    //endregion Listener for StackView
+
 }
