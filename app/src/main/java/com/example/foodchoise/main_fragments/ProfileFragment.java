e4b8fa6a5bf1e790770b0b62e5f5cc537e2f49fa
vladcto <razrab.ytka@gmail.com>
2020-05-29 19:40:29 +0300

    Сделал профиль. Большего не будет т.к. нехватка времени.

--- app/src/main/java/com/example/foodchoise/main_fragments/ProfileFragment.java
+++ app/src/main/java/com/example/foodchoise/main_fragments/ProfileFragment.java
@@ -9,12 +9,20 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodchoise.R;
+import com.example.foodchoise.entity_classes.AdapterBuilder;
+import com.example.foodchoise.entity_classes.BriefRecipeCardAdapter;
+import com.example.foodchoise.helperFirebase.database.FirestoreHelper;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class ProfileFragment extends Fragment {
 
+    BriefRecipeCardAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,6 +37,25 @@ public class ProfileFragment extends Fragment {
                 }catch (NullPointerException ignored){}
             }
         });
+
+        Query query = FirebaseFirestore.getInstance().collection(FirestoreHelper.COLLECTION_RECIPES)
+                .whereEqualTo("author", FirebaseAuth.getInstance().getCurrentUser().getUid());
+        adapter = AdapterBuilder.getBriefRecipeAdapter(getActivity(), query);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
